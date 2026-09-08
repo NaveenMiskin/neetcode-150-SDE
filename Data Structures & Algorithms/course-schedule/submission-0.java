3b51b04db47b06a67e8e9e83b9bed26e499658ae
Naveen Miskin <153484070+NaveenMiskin@users.noreply.github.com>
2026-09-09 00:29:57 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(v).add(u);
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            for(int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int i = 0;
+        int[] topo = new int[numCourses];
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            topo[i++] = node;
+
+            for(int it : adj.get(node)) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+        if(i == numCourses) return true;
+        return false;
+    }
+}
